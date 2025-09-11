e3c7d8ac79d0693e5e76f5a719cd8dc9a306bb87
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-10 20:41:14 -0500

    looking into the reference equals method.

--- Program.cs
+++ Program.cs
@@ -1,81 +1,42 @@
-﻿// var point = new Point(10, 20);
-// var pointMoved = point with { X = point.X + 1 };
+﻿var john = new Person("John", 1);
+var theSameAsJohn = john;
+System.Console.WriteLine(object.ReferenceEquals(john, theSameAsJohn));
+theSameAsJohn = new Person("John", 1);
+System.Console.WriteLine(object.ReferenceEquals(john, theSameAsJohn));
 
-// var person = new Person("Bob", 1);
-// var person2 = person with { Id = 2 };
-
-// var dateTime = new DateTime(2023, 6, 7);
-// var dateWeekAfter = dateTime.AddDays(7);
-
-// var pet = new { Name = "Barbara", Type = "Fish" };
-// var asCrab = pet with { Type = "Crab" };
-
-
-// Console.ReadKey();
-
-// struct Point
-// {
-//   public int X { get; init; }
-//   public int Y { get; init; }
-
-//   public Point()
-//   {
-//     X = 0;
-//     Y = 1;
-//   }
-
-//   public Point(int x, int y)
-//   {
-//     X = x;
-//     Y = y;
-//   }
-
-//   public override string ToString() => $"X: {X}, Y: {Y}";
-// }
-
-// class Person
-// {
-//   private Point _favouritePoint;
-//   private Person _favouritePerson;
-//   public int Id { get; set; }
-//   public string Name { get; init; }
-
-//   public Person(string name, int id)
-//   {
-//     Name = name;
-//     Id = id;
-//   }
-// }
-var point = new Point { X = 1, Y = 1 };
-var pointButFarAway = point with { X = 100 };
-
-var tacoBell = new Taco { X = 5, Y = 3 };
-
-System.Console.WriteLine(point.X);
-System.Console.WriteLine(pointButFarAway.X);
 
 Console.ReadKey();
 
-public struct Point
+struct Point
 {
   public int X { get; init; }
   public int Y { get; init; }
+
+  public Point()
+  {
+    X = 0;
+    Y = 1;
+  }
 
   public Point(int x, int y)
   {
     X = x;
     Y = y;
   }
+
+  public override string ToString() => $"X: {X}, Y: {Y}";
 }
 
-public class Taco
+class Person
 {
-  public int X { get; init; }
-  public int Y { get; init; }
+  private Point _favouritePoint;
+  private Person _favouritePerson;
+  public int Id { get; set; }
+  public string Name { get; init; }
 
-  public Taco(int x, int y)
+  public Person(string name, int id)
   {
-    X = x;
-    Y = y;
+    Name = name;
+    Id = id;
   }
 }
