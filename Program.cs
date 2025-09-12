afb171a24bfd4e753fe12f45d6e3c334ba00fd86
Reid Herrera <reidherrerabusiness@gmail.com>
2025-09-11 19:14:44 -0500

    learning about using nullables reference types . generic type constraints related to nullibility

--- Program.cs
+++ Program.cs
@@ -1,42 +1,56 @@
-﻿System.Console.WriteLine("Went over the null forgiving operator");
+﻿// System.Console.WriteLine("Went over the null forgiving operator");
+// Console.ReadKey();
+
+// class House
+// {
+//   public string OwnerName { get; }
+//   public Address Address { get; }
+
+//   public House(string ownerName, Address address)
+//   {
+//     if (ownerName == null)
+//     {
+//       throw new ArgumentNullException(nameof(ownerName));
+//     }
+//     if (address == null) {
+//       throw new ArgumentNullException(nameof(address));
+//     }
+//     OwnerName = ownerName;
+//     Address = address;
+//   }
+// }
+
+// class Address
+// {
+//   public string Street { get; }
+//   public string Number { get; }
+
+//   public Address(string street, string number)
+//   {
+//     if (street == null)
+//     {
+//       throw new ArgumentNullException(nameof(street));
+//     }
+//     if (number == null)
+//     {
+//       throw new ArgumentNullException(nameof(number));
+//     }
+//     Street = street;
+//     Number = number;
+
+//   }
+// }
+
+#nullable disable
+string text = null;
+
+#nullable enable
+string? otherText = null;
+SomeMethod(otherText);
+
 Console.ReadKey();
 
-class House
+void SomeMethod<T>(T input) where T : class?
 {
-  public string OwnerName { get; }
-  public Address Address { get; }
-
-  public House(string ownerName, Address address)
-  {
-    if (ownerName == null)
-    {
-      throw new ArgumentNullException(nameof(ownerName));
-    }
-    if (address == null) {
-      throw new ArgumentNullException(nameof(address));
-    }
-    OwnerName = ownerName;
-    Address = address;
-  }
-}
-
-class Address
-{
-  public string Street { get; }
-  public string Number { get; }
-
-  public Address(string street, string number)
-  {
-    if (street == null)
-    {
-      throw new ArgumentNullException(nameof(street));
-    }
-    if (number == null)
-    {
-      throw new ArgumentNullException(nameof(number));
-    }
-    Street = street;
-    Number = number;
-    
-  }
+  
 }
